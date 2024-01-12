5fb82d9e9c7d15a75e72a4a27ab252af381bbbbd
Vulpes231 <whykayshot@gmail.com>
2024-01-12 14:00:03 +0100

    initial commit

---
+++ src/utils/mailer.js
@@ -0,0 +1,39 @@
+const nodemailer = require("nodemailer");
+
+const mailer = (email, message, subject) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.forwardemail.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: email,
+    subject: subject,
+    html: `
+        <body>
+          <div>
+            <img style='background-color: white;' src='https://res.cloudinary.com/dcpmainhy/image/upload/v1680959833/qvfcwq4bzdbfcginvqmc.png' height=150px width=100% alt='web_logo'/>
+          </div>
+          <div>
+            ${message}
+          </div>
+        </body>
+      `,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+      console.log(info);
+    }
+  });
+};
+
+module.exports = { mailer };
